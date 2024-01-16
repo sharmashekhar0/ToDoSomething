0ff99f32424faf2144ac8fe91f37688f762ef7b6
sharmashekhar0 <beingperfect76@gmail.com>
2024-01-16 23:15:36 +0530

    add login

---
+++ src/components/Login.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { login as authLogin } from "../store/authSlice";
+import { useDispatch } from "react-redux";
+import authService from "../appwrite/authService";
+import { useForm } from "react-hook-form";
+
+function Login() {
+	const [error, setError] = useState("");
+	const navigate = useNavigate();
+	const { register, handleSubmit } = useForm();
+	const dispatch = useDispatch();
+
+	const login = async (data) => {
+		setError("");
+		console.log("Data", data);
+		try {
+			const session = await authService.login(data);
+			if (session) {
+				const userData = await authService.getCurrentUser();
+				if (userData) {
+					dispatch(authLogin(userData));
+					navigate("/");
+				}
+			}
+		} catch (error) {
+			setError(error.message);
+		}
+	};
+
+	return (
+		<div className="flex items-center justify-center w-full">
+			<div
+				className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+			>
+				<div className="mb-2 flex justify-center">
+					<span className="inline-block w-full max-w-[100px]">
+						<p>Logo</p>
+					</span>
+				</div>
+				<h2 className="text-center text-2xl font-bold leading-tight">
+					Sign in to your account
+				</h2>
+				<p className="mt-2 text-center text-base text-black/60">
+					Don&apos;t have any account?&nbsp;
+					<Link
+						to="/signup"
+						className="font-medium text-primary transition-all duration-200 hover:underline"
+					>
+						Sign Up
+					</Link>
+				</p>
+				{error && (
+					<p className="text-red-600 mt-8 text-center">{error}</p>
+				)}
+				<form onSubmit={handleSubmit(login)} className="mt-8">
+					<div className="space-y-5 flex flex-col">
+						<div className="flex gap-8 items-center">
+							<label htmlFor="">Email</label>
+							<input
+								type="email"
+								placeholder="Enter your email"
+								{...register("email", {
+									required: true,
+									validate: {
+										matchPatern: (value) =>
+											/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(
+												value
+											) ||
+											"Email address must be a valid address",
+									},
+								})}
+							/>
+						</div>
+						<div className="flex gap-8 items-center">
+							<label htmlFor="">Password</label>
+							<input
+								type="password"
+								placeholder="Enter your password"
+								{...register("password", {
+									required: true,
+								})}
+							/>
+						</div>
+						<button type="submit" className="w-full">
+							Sign in
+						</button>
+					</div>
+				</form>
+			</div>
+		</div>
+	);
+}
+
+export default Login;
